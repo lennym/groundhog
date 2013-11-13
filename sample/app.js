eeed36e12733df2c2ae4163a437be383c927f722
Nuno Job <nunojobpinto@gmail.com>
2013-11-13 18:21:59 +0000

    Changes structure of conf file
    * Should mostly work now

---
+++ sample/app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var request = require('request');
+
+var config = require('./config');
+var host = config.host;
+var port = config.port;
+
+http.createServer(function (req, res) {
+  console.log(host + req.url);
+  //
+  // proxy request to emulate expected behavior
+  // should be the same as groundhog since groundhog records
+  // proxied stuff
+  //
+  req.pipe(request(host + req.url)).pipe(res);
+}).listen(port, '127.0.0.1');
+
+console.log('ready');
+
+setTimeout(function () {
+  console.log('bye bye');
+}, 500);
---
+++ sample/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  host: process.env._HOST || 'yld.io',
+  port: process.env._PORT || 1337
+};
